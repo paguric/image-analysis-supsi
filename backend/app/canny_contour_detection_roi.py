136a41b045414c74185ea64c9cd4813a8b6a1700
paguric <lpernisi05@gmail.com>
2026-03-02 12:04:37 +0100

    spostato script SoA in backend/app

---
+++ backend/app/canny_contour_detection_roi.py
@@ -0,0 +1,72 @@
+import cv2
+import av
+import time
+
+
+def time_convert(seconds):
+    mins = seconds // 60
+    secs = seconds % 60
+    return f"{int(mins)}:{int(secs):02d}"
+
+def canny_contour_detection(video_to_analyze_path):
+
+    # Apertura video
+    container = av.open(video_to_analyze_path)
+    stream = container.streams.video[0]
+
+    start_perf = time.perf_counter()
+    start_time = time.time()
+
+    cv2.namedWindow("video", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("video", 1400, 800)
+
+    # Riproduzione frame-by-frame
+    for frame in container.decode(stream):
+
+        # PyAV → OpenCV
+        img = frame.to_ndarray(format="bgr24")
+
+        # timestamp reale del frame
+        t = float(frame.pts * frame.time_base)
+
+        # PREPROCESSING
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # aumento contrasto locale
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        gray = clahe.apply(gray)
+
+        # riduzione rumore
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # CANNY EDGE DETECTION
+        edges = cv2.Canny(
+            blur,
+            threshold1=50,
+            threshold2=150
+        )
+
+        # CONTORNI
+        contours, _ = cv2.findContours(
+            edges,
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE
+        )
+
+        # disegno contorni
+        output = img.copy()
+        cv2.drawContours(output, contours, -1, (0, 255, 0), 2)
+
+        # Sync realtime
+        while time.perf_counter() - start_perf < t:
+            time.sleep(0.001)
+
+        cv2.imshow("video", output)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    end_time = time.time()
+    print("Durata video:", time_convert(end_time - start_time))
+
+    cv2.destroyAllWindows()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app import video_reading
+from app import roi_identification
+from app import canny_contour_detection_roi
+
 app = FastAPI()
 
 app.add_middleware(
@@ -14,3 +18,19 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+def main():
+    #video_paths = ["video/prima.avi", "video/dopo.avi"]
+    #print(f"Does the two videos have the same lenght? {video_reading.check_frames_number(video_paths)}")
+
+    video_to_analyze_path = "video/dopo.avi"
+    
+    # <-- EDGE DETECTION -->
+    #roi_identification.compute_roi(video_to_analyze_path)
+
+    # <-- CONTOUR DETECTION -->
+    canny_contour_detection_roi.canny_contour_detection(video_to_analyze_path)
+
+if __name__ == "__main__":
+    main()
