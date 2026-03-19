e95a9e1c603e9d3a18598362a99df77a6ca4cbde
paguric <lpernisi05@gmail.com>
2026-03-19 16:32:27 +0100

    completato endpoint /analyze

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
+import os
 from fastapi import FastAPI, UploadFile, File
+from app import pipeline
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -16,9 +19,29 @@ async def root():
     return {"message": "Hello World"}
 
 
+OUTPUT_DIR = "out"
+
+
 @app.post("/analyze")
 async def analyze(
     video_prima: UploadFile = File(...), video_dopo: UploadFile = File(...)
 ):
     prima_bytes = await video_prima.read()
     dopo_bytes = await video_dopo.read()
+
+    prima_path = f"{OUTPUT_DIR}/prima.avi"
+    dopo_path = f"{OUTPUT_DIR}/dopo.avi"
+    diff_path = f"{OUTPUT_DIR}/diff.avi"
+
+    with open(prima_path, "wb") as f:
+        f.write(prima_bytes)
+    with open(dopo_path, "wb") as f:
+        f.write(dopo_bytes)
+
+    pipeline.analyze(prima_path, dopo_path, diff_path)
+
+    return {
+        "video_prima_url": prima_path,
+        "video_dopo_url": dopo_path,
+        "video_diff_url": diff_path,
+    }
