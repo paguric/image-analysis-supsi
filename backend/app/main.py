b76d4e51662d2cae6e5e4386a80af42774dd8e68
paguric <lpernisi05@gmail.com>
2026-03-19 15:25:33 +0100

    rimossi import non necessari da main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import cv2
-
-from app import preprocessing
-from app import cv2_utils
-from app import norm
-from app import video_reading
-from app import edge_detection
-from app import contour_detection
-from app import canny_contour_detection_roi
-from app import watershed
-
 app = FastAPI()
 
 app.add_middleware(
