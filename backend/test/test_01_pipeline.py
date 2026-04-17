3a50c5fae64d814cb715e24c91285597e66c4716
paguric <lpernisi05@gmail.com>
2026-04-17 12:14:18 +0200

    feat(test): Aggiunge creazione di clip per velocizzare test (al momento però non funziona)

--- backend/test/test_01_pipeline.py
+++ backend/test/test_01_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import unittest
 import pytest
 import utils
@@ -6,6 +7,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 from pydantic_core import from_json
 from app.schemas.roi import RoiResponse
+from app.services import video_metadata_service
 
 
 class PipelineControllerTest(unittest.TestCase):
@@ -18,9 +20,6 @@ class PipelineControllerTest(unittest.TestCase):
         self.video_prima = "/home/lapo225/università/as25-26-sp/prog-semestre/00_image_analysis_unict/dopo.avi"
         self.video_dopo = "/home/lapo225/università/as25-26-sp/prog-semestre/00_image_analysis_unict/prima.avi"
 
-        # Crea una copia tagliata del video, in modo che i test eseguano più velocemente
-        self.total_frames = 20
-
         os.makedirs("out", exist_ok=True)
         os.makedirs("out/diff", exist_ok=True)
         os.makedirs("out/roi", exist_ok=True)
@@ -28,6 +27,50 @@ class PipelineControllerTest(unittest.TestCase):
         os.makedirs("out/roi/dopo", exist_ok=True)
         os.makedirs("out/roi/prima/steps", exist_ok=True)
         os.makedirs("out/roi/dopo/steps", exist_ok=True)
+
+        # Non capisco perchè, ma se genero una clip non trova più un paio di ROI
+        return
+
+        # Crea una copia tagliata del video, in modo che i test eseguano più velocemente
+        self.total_frames = 260
+
+        # Estrae una clip di lunghezza (in frame) self.total_frames attorno al frame più luminoso del video dopo
+        # Per non dover eseguire video_metadata_service.brightest_frame sul video prima (non avrebbe più senso il taglio a quel punto),
+        # Imposto manualmente il frame più luminoso
+
+        # print(video_metadata_service.brightest_frame(self.video_dopo))
+        # print(video_metadata_service.brightest_frame(self.video_prima)) -> 165
+        self.brightest_idx = 161
+
+        os.makedirs("./test", exist_ok=True)
+        self.video_prima_clip = "./test/prima_clip.avi"
+        self.video_dopo_clip = "./test/dopo_clip.avi"
+
+        if os.path.exists(self.video_prima_clip) and os.path.exists(
+            self.video_dopo_clip
+        ):
+            return
+
+        half = self.total_frames // 2
+        start = self.brightest_idx - half
+
+        def _cut(src, dst):
+            cap = cv2.VideoCapture(src)
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            w, h = (
+                int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+            )
+            out = cv2.VideoWriter(dst, cv2.VideoWriter_fourcc(*"XVID"), fps, (w, h))
+            cap.set(cv2.CAP_PROP_POS_FRAMES, start)
+            for _ in range(self.total_frames):
+                ret, frame = cap.read()
+                out.write(frame)
+            cap.release()
+            out.release()
+
+        _cut(self.video_dopo, self.video_dopo_clip)
+        _cut(self.video_prima, self.video_prima_clip)
 
     def tearDown(self):
         return None
@@ -51,8 +94,8 @@ class PipelineControllerTest(unittest.TestCase):
         response = self.client.post(
             "/pipeline/local/",
             json={
-                "video_prima": self.video_prima,
-                "video_dopo": self.video_dopo,
+                "video_prima": self.video_prima_clip,
+                "video_dopo": self.video_dopo_clip,
             },
         )
 
@@ -64,7 +107,7 @@ class PipelineControllerTest(unittest.TestCase):
                 "/roi/prima/",
                 json={
                     "index": i,
-                    "frame": 150,
+                    "frame": self.total_frames // 2,
                 },
             )
 
@@ -77,7 +120,7 @@ class PipelineControllerTest(unittest.TestCase):
                 "/roi/dopo/",
                 json={
                     "index": i,
-                    "frame": 150,
+                    "frame": self.total_frames // 2,
                 },
             )
 
@@ -87,23 +130,28 @@ class PipelineControllerTest(unittest.TestCase):
         """
         Verifica /pipeline/diff/{frame}/ (differenziale globale).
         """
-        response = self.client.get("/pipeline/diff/100/")
+        response = self.client.get(f"/pipeline/diff/{self.total_frames // 2}/")
 
         assert response.status_code == 200
         assert response.headers["content-type"] == "image/jpeg"
 
-        utils.save_image(response.content, "out/diff/diff_frame_100.jpg")
+        utils.save_image(
+            response.content, f"out/diff/diff_frame_{self.total_frames // 2}.jpg"
+        )
 
     def test_get_diff_with_contours(self):
         """
         Verifica /pipeline/diff/{frame}/contours/ (differenziale globale con contorni).
         """
-        response = self.client.get("/pipeline/diff/100/contours/")
+        response = self.client.get(f"/pipeline/diff/{self.total_frames // 2}/contours/")
 
         assert response.status_code == 200
         assert response.headers["content-type"] == "image/jpeg"
 
-        utils.save_image(response.content, "out/diff/diff_frame_100_contours.jpg")
+        utils.save_image(
+            response.content,
+            f"out/diff/diff_frame_{self.total_frames // 2}_contours.jpg",
+        )
 
     def test_get_roi_prima_steps(self):
         """
