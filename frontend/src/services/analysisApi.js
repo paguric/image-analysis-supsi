1bb4a4e35edf1009e7507cfc8c956852da1df058
Simone-03 <simone.torri03@gmail.com>
2026-04-23 10:41:34 +0200

    Inizio fix documentazione automatica e aggiunta documentazione frontend

--- frontend/src/services/analysisApi.js
+++ frontend/src/services/analysisApi.js
@@ -2,6 +2,10 @@ import { BASE_URL } from '../constants/BaseUrl'
 import { clearTemporaryParametrization } from './temporaryParameters'
 
 
+/**
+ * @description This function triggers the clear database function
+ * in the backend and the clear of the temporary analysis parametrization.
+ */
 export async function clearDataBase() {
     const response = await fetch(`${BASE_URL}/analysis/reset/`, {
         method: 'DELETE',
@@ -14,7 +18,11 @@ export async function clearDataBase() {
     }
 }
 
-
+/**
+ * @description Function to handle the csv export response 
+ * @param {*} response takes the response returned from the backend call
+ * @returns returns the analyzed result from the backend call
+ */
 async function handleExportResponse(response) {
     if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
@@ -30,13 +38,23 @@ async function handleExportResponse(response) {
     return result;
 }
 
-
+/**
+ * @description Function to handle the pixel-by-pixel, single frame CSV export
+ * @param {*} frameToExport takes as parameter the frame you want to export
+ * @returns returns the analyzed result from the backend call
+ */
 export async function exportFrameToCSV(frameToExport) {
     const response = await fetch(`${BASE_URL}/analysis/export-csv/diff/frame/${frameToExport}/pixels/`);
     return handleExportResponse(response);
 }
 
 
+/**
+ * @description Function to handle the global CSV export
+ * @param {*} minWavelength the starting wavelength
+ * @param {*} maxWavelength the final wavelength
+ * @returns returns the analyzed result from the backend call
+ */
 export async function exportGlobalToCSV(minWavelength, maxWavelength) {
     const response = await fetch(`${BASE_URL}/analysis/diff/results/${minWavelength}/${maxWavelength}/`);
     return handleExportResponse(response);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import { BASE_URL } from  '../constants/BaseUrl'
 
+
+/**
+ * @description Crea un collegamento con il backend
+ */
 const api = axios.create({ baseURL: BASE_URL });
 
+
+/**
+ * @description Function that manages the images retrieved from the backend 
+ * @param {*} url The URL to the backend method endpoint
+ * @param {*} options 
+ * @returns Restituisce in url per utilizzare l'immagine
+ */
 export async function fetchImage(url, options = {}) {
   const res = await fetch(url, options);
   if (!res.ok) {
@@ -16,6 +27,10 @@ export async function fetchImage(url, options = {}) {
   return URL.createObjectURL(blob);
 }
 
+/**
+ * @description This function allows to wait a certaing amount of time before executing the next line 
+ * @param {*} ms The amount of time you want to wait (in milliseconds)
+ */
 export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default api;
